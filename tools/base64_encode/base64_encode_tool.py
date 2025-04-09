1fb162215f9e428077739342ede3eddc3f10129e
David <david.thomfohrde@gmail.com>
2025-04-09 15:28:38 +0200

    Die Basis für mehrere Sprachen hinzugefügt.

--- tools/base64_encode/base64_encode_tool.py
+++ tools/base64_encode/base64_encode_tool.py
@@ -3,35 +3,35 @@ from tool_interface import MiniTool
 
 class Base64EncodeTool(MiniTool):
     def __init__(self):
-        super().__init__("Base64 Encode Tool", "Base64EncodeTool")
+        super().__init__("Base64 Kodierungstool", "Base64EncodeTool")
         self.input_params = {
-            "Text to encode": "string",
-            "Encoding": {
+            "Zu kodierender Text": "string",
+            "Kodierung": {
                 "type": "enum",
                 "options": ["utf-8", "ascii"]
             }
         }
-        self.description = "Encodes any input text to a Base64 String. Supports ascii and utf-8 encoding"
-        
+        self.description = "Kodiert jeden Eingabetext in einen Base64-String. Unterstützt ASCII- und UTF-8-Kodierung"
+
     def execute_tool(self, input_params: dict) -> bool:
         try:
-            message = input_params.get("Text to encode", "")
-            encoding = input_params.get("Encoding", "utf-8")
-            
+            message = input_params.get("Zu kodierender Text", "")
+            encoding = input_params.get("Kodierung", "utf-8")
+
             if message == "":
-                self.error_message = "Input text is empty or invalid"
+                self.error_message = "Eingabetext ist leer oder ungültig"
                 return False
-                
+
             message_bytes = message.encode(encoding)
             base64_bytes = base64.b64encode(message_bytes)
-            # utf-8 is needed for web-compatible display,
-            # and it's backwards compatible with
-            # ascii anyway
+            # utf-8 ist nötig für webkompatible Darstellung
+            # und ist ohnehin abwärtskompatibel mit
+            # ascii
             base64_message = base64_bytes.decode('utf-8')
-            
+
             self.output = base64_message
             return True
-            
+
         except Exception as e:
             print(str(e))
             self.error_message = str(e)
